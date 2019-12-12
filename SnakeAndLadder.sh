2baf5578e6f15ae458143fed00bd9fa6bcd9e4c1
Prashant2411 <prashantbedi241197@gmail.com>
2019-12-12 16:51:20 +0530

    Added: Computing the next move of user

--- SnakeAndLadder.sh
+++ SnakeAndLadder.sh
@@ -5,3 +5,17 @@ echo "Welcome to Snake and Ladder."
 position=0
 
 diceNumber=$(( RANDOM % 6 + 1 ))
+
+function getResult () {
+	nextMove=$(( RANDOM % 3 ))
+	case $nextMove in
+		2 )
+			position=$(( $position - diceNumber ));;
+		1 )
+			position=$(( $position + diceNumber ));;
+		0 )
+			;;
+	esac
+}
+
+getResult
